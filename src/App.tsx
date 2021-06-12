47dbcf1641f43d4edc60e2e23170e51b9be70d40
MarcusSlover <mariuszek016@gmail.com>
2021-06-12 02:19:08 +0200

    + init

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    const
+        home = "home(\"marcus\")",
+        creations = "creations()",
+        github = "github()",
+        about = "about()";
+
+    const logo = "https://crafatar.com/renders/body/398c7569-3f15-4998-b0d9-be91a9ac935c?overlay&scale=10";
+
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+
+            {/*Website header*/}
+            <header className={"header"}>
+
+                {/*Navigation bar*/}
+                <div className={"navigation"}>
+                    <nav>
+                        <ul>
+                            <li><a href={"#home"}>
+                                <div>{home}</div>
+                            </a></li>
+                            <li><a href={"#about"}>
+                                <div>{about}</div>
+                            </a></li>
+                            <li><a href={"#creations"}>
+                                <div>{creations}</div>
+                            </a></li>
+                            <li><a href={"#github"}>
+                                <div>{github}</div>
+                            </a></li>
+                        </ul>
+                    </nav>
+                </div>
+
+            </header>
+
+            <header className={"about"}>
+
+                <span className={"upper"}>
+
+                    <section className={"figure"}>
+                        <img src={logo}/>
+                    </section>
+
+                    <section className={"box"}>
+                    </section>
+
+                    <section className={"about-text"}>
+                        <text>{"\""}</text>
+                        <br/>
+                        <text>  Hi! I am Marcus.</text>
+                        <br/>
+                        <text>  I'm 18 years old and programming is my passion."</text>
+                        <br/>
+                        <text>{"\""}</text>
+                    </section>
+                </span>
             </header>
         </div>
     );
