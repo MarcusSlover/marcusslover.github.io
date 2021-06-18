666085113d07500904aa4a37ccf17ce1ea410833
MarcusSlover <mariuszek016@gmail.com>
2021-06-18 03:42:15 +0200

    + website

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,15 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from "react-router-dom";
+
 import App from './components/app/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-    <React.StrictMode>
+    <BrowserRouter>
         <App/>
-    </React.StrictMode>,
+    </BrowserRouter>,
     document.getElementById('root')
 );
 
